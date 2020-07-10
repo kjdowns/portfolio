46127c1d25a7c8c2c1d65ef9419eeee2d2a88e5f
kjdowns <kevin.downs04@gmail.com>
2020-07-10 17:43:52 -0400

    add state for form submission

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class Contact extends Component {
+
+   constructor(){
+      super()
+      this.state = {
+         contactName: "",
+         contactEmail: "",
+         contactSubject: "",
+         contactMessage: ""
+      }
+   }
+
   render() {
 
     if(this.props.data){

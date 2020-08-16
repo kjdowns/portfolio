79de7da41b3d4bd8d5a6255a991e771da301146f
kjdowns <kevin.downs04@gmail.com>
2020-08-15 21:40:11 -0400

    add learning skills section listing currently learning tech

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,6 +5,7 @@ class Resume extends Component {
 
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
+      var learningmessage = this.props.data.learningmessage;
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
@@ -19,6 +20,10 @@ class Resume extends Component {
       var skills = this.props.data.skills.map(function(skills){
         var className = 'bar-expand '+skills.name.toLowerCase();
         return <div key={skills.name} className="tech"><img alt="" width="100" height="100" src={'/images/icons/' + skills.icon}></img><h5>{skills.name}</h5></div>
+      })
+      var learningskills = this.props.data.learningskills.map(function(learningskills){
+        var className = 'bar-expand '+learningskills.name.toLowerCase();
+        return <div key={learningskills.name} className="tech"><img alt="" width="100" height="100" src={'/images/icons/' + learningskills.icon}></img><h5>{learningskills.name}</h5></div>
       })
     }
 
@@ -53,7 +58,7 @@ class Resume extends Component {
 
 
 
-      <div className="row skill">
+      <div className="row skill" >
 
          <div className="three columns header-col">
             <h1><span>Skills</span></h1>
@@ -67,6 +72,25 @@ class Resume extends Component {
 				<div className="bars">
 				   {/* <ul className="skills"> */}
 					  {skills}
+					{/* </ul> */}
+				</div>
+			</div>
+      </div>
+
+      <div className="row skill">
+
+         <div className="three columns header-col">
+            <h1><span>Learning</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+
+            <p>{learningmessage}
+            </p>
+
+				<div className="bars">
+				   {/* <ul className="skills"> */}
+					  {learningskills}
 					{/* </ul> */}
 				</div>
 			</div>
